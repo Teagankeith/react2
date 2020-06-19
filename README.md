44b2092ff307edaef5529b943964c0824a53537e
Teagan Keith <teagan-keith@lambdastudents.com>
2020-06-19 23:57:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+- Dependencies/Frameworks used in this React App
+    - CRA
+    - Axios
+    - Redux / React-redux
+    - YUP
+
+- Project Link : watermyplants-app.now.sh
+
+
---
+++ dashboard/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Water My Plants, React 2, 6/19/2020</h2>
+    </div>
+  );
+}
+
+export default App;
